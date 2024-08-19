c86e9bea06958f3a103f4aa031c52a1c8f260c4e
vesnx\wave <wave@vesnx.com>
2024-08-19 13:56:43 +0200

    update NuGet package

--- samples/Demo/SecureJson/obj/Debug/net8.0/SecureJson.AssemblyInfo.cs
+++ samples/Demo/SecureJson/obj/Debug/net8.0/SecureJson.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("2024 Walter Verhoeven, all rights reserved")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Test application showcasing features")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d38256496669d9259bdd88cacf8bddb79f45a1c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df40c0299bd5948003afba02377e7792f1543fe9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecureJson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecureJson")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
